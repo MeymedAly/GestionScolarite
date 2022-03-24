0e24013559e534bdc0dde97ecae3196ee5c34047
meymed2727 <meymed.aly@gmail.com>
2022-03-24 23:23:47 +0000

    des modifications page d'acceill et navbar et sidebar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  Link='assets/home3.jpg'
+  Link='assets/sco.jpeg'
+
   
   
   constructor() { }
